bdf8214e219575e17aad39f200968cdae5817ec6
Hauke Hans <hauke.hans@eventim.de>
2021-12-10 10:47:36 +0100

    cleanup

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,47 +1,41 @@
 import scala.annotation.tailrec
 import scala.collection.mutable
 
-case object Incomplete
-
 object Day10 {
 
   def main(args: Array[String]): Unit = {
     val input =
       readLines("input/day10.txt").map(_.trim).map(_.toCharArray.toList)
 
-    val parsed = input.map(l => parse(l, List.empty))
+    val parsed = input.map(parse(_, List.empty))
 
-    val part1 = parsed.collect { case Right(Some(badChar)) =>
-      badChar match {
-        case ')' => 3
-        case ']' => 57
-        case '}' => 1197
-        case '>' => 25137
-      }
+    val part1 = parsed.collect {
+      case Right(Some(')')) => 3
+      case Right(Some(']')) => 57
+      case Right(Some('}')) => 1197
+      case Right(Some('>')) => 25137
     }.sum
 
     println(part1)
 
-    val incomplete = input.map(parse(_, List.empty)).collect {
-      case Left(chars) => chars
-    }
-
-    val completionScores = incomplete.map { l =>
-      val s = l.map {
-        case '(' => 1L
-        case '[' => 2L
-        case '{' => 3L
-        case '<' => 4L
+    val completionScores = input
+      .map(parse(_, List.empty))
+      .collect { case Left(chars) => chars }
+      .map {
+        _.map {
+          case '(' => 1L
+          case '[' => 2L
+          case '{' => 3L
+          case '<' => 4L
+        }.foldLeft(0L) { (score, next) =>
+          5L * score + next
+        }
       }
-      s.foldLeft(0L) { (score, next) =>
-        5L * score + next
-      }
-    }.sorted
+      .sorted
 
     val part2 = completionScores((completionScores.length - 1) / 2)
 
     println(part2)
-
   }
 
   @tailrec
@@ -64,7 +58,7 @@ object Day10 {
               case Some('<') if c == '>' => parse(tail, stack.tail)
               case Some('(') if c == ')' => parse(tail, stack.tail)
               case Some(other)           => Right(Some(head))
-              case None                  => Left(stack) // ???
+              case None                  => Left(stack)
             }
         }
     }
