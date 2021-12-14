77c9f1878288aa07959a2615db38fde8fc96420d
Hauke Hans <hauke.hans@eventim.de>
2021-12-14 20:42:31 +0100

    d14

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -13,27 +13,44 @@ object Day14 {
         (a, b) -> add.toCharArray.head
       }
       .toMap
+
     val pairs = template.zip(template.drop(1))
 
-    val res = expand(template.toCharArray, rules, 2)
-
-    println(res)
-  }
-
-//  @tailrec
-  def expand(template: Array[Char], rules: Map[(Char,Char), Char], iterations: Int, round: Int = 1): Unit = {
-    if (round == iterations) template.mkString("") else {
-      val pairs = template.sliding(2).take(3)
-
-      println(pairs.map(_.mkString("Array(", ", ", ")")).mkString(""))
-//      val c = pairs.flatMap { p =>
-//        rules.get(p).map(i => Array[Char](p._1, i, p._2)).getOrElse(Array[Char](p._1, p._2))
-//      }
-//
-//      expand(c, rules, iterations, round + 1)
+    @tailrec
+    def loop(input: Array[Char], limit: Int, i: Int = 0): String = {
+      if (i == limit) input.mkString
+      else {
+        val res = expand(input, rules, 0)
+        loop(res.toCharArray, limit, i + 1)
+      }
     }
 
+    val res = loop(input.head.toCharArray, 40)
 
+    val x = res.toCharArray.groupBy(identity).view.mapValues(_.length)
+    val max = x.maxBy(_._2)
+    val min = x.minBy(_._2)
+
+    println(max._2 - min._2)
+  }
+
+  @tailrec
+  def expand(
+      template: Array[Char],
+      rules: Map[(Char, Char), Char],
+      pos: Int
+  ): String = {
+    if (pos == template.length - 1) {
+      template.mkString
+    } else {
+      val Array(a, b) = template.slice(pos, pos + 2)
+      val nextT = rules.get((a, b)).fold(template) { newChar =>
+        val (head, tail) = template.splitAt(pos + 1)
+        val next = head ++ Array(newChar) ++ tail
+        next
+      }
+      expand(nextT, rules, pos + 2)
+    }
   }
 
 }
